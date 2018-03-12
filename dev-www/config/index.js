27eabd35eb936f2ef33e1a694aa1b6489cfc19dc
a <a@qq.com>
2018-03-12 13:59:33 +0800

    build

--- dev-www/config/index.js
+++ dev-www/config/index.js
@@ -49,9 +49,9 @@ module.exports = {
     index: path.resolve(__dirname, '../../views/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../../public'),
+    assetsRoot: path.resolve(__dirname, '../public'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
